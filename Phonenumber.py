7e25c243af54848775480ab96925808426f278b0
NightCoderAmit <87881212+NightCoderAmit@users.noreply.github.com>
2021-07-29 11:20:01 +0530

    Create Phonenumber.py
    This a fakecode

---
+++ Phonenumber.py
@@ -0,0 +1,12 @@
+import phonenumbers
+number = input("Please enter the phone number with country code: ")
+
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number , "CH")
+print("The origin of the phone number is: ")
+print(geocoder.description_for_number(ch_number , "en"))
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number , "RO")
+print("The service provider of your number is: ")
+print(carrier.name_for_number(service_provider , "en"))
